df9890d6a6c5cf56b7e56eff102208ac56ed61aa
Gustav Larsson <gustav.m.larsson@gmail.com>
2015-10-11 14:20:38 -0500

    Started 0.1.1

--- crox/__init__.py
+++ crox/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import print_function, division, absolute_import
 
 
-VERSION = (0, 1, 0)
-ISRELEASED = True
+VERSION = (0, 1, 1)
+ISRELEASED = False
 __version__ = '{0}.{1}.{2}'.format(*VERSION)
 if not ISRELEASED:
     __version__ += '.git'
